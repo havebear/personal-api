a361c622b39a2eae6eee7c1c2ad6c387befb37c1
xbb <616749285@qq.com>
2025-07-04 18:38:59 +0800

    feat: 更新 doc

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,8 @@ async function bootstrap() {
   // CORS
   app.enableCors();
   
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
-bootstrap(); 
+bootstrap();
